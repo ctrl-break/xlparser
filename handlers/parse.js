76cc744e3cf002658d94e421da58de1988375aaa
OlgaKuzminova <ctrl-break@mail.ru>
2023-04-06 18:56:07 +0300

    fuzzy search cultivar id by name

--- handlers/parse.js
+++ handlers/parse.js
@@ -1,28 +1,52 @@
 const XLSX = require("xlsx");
 const store = require('./store');
 const schemas = require('./templates');
+const { fuzzy } = require("fast-fuzzy");
 
 const isCultivarMeasureSchema1 = (rows) => ( rows[1] && typeof rows[1] === 'string' && rows[2] >=0 && typeof rows[2] === 'number');
 const getRawData = (row) => row.filter((item, index) => index <= 11);
-const normalizeCultivarName = (cultivarName) => cultivarName ? cultivarName.replace('|Г|', '').trim().toLowerCase().replace('ё', 'е') : '';
+const normalizeCultivarName = (cultivarName) => cultivarName 
+    ? cultivarName.replace('|Г|', '').trim().toLowerCase().replace('ё', 'е') 
+    : '';
 
 const getCultivarDbId = (row) => {
     if (!row || row.length === 0) {
         return;
     }
-    const cultivars =   store.getItem('cultivars');
+    const cultivars = store.getItem('cultivars');
+    const cultivarName = row[1]['value'];
+
     // add templates indexes
     const currentCultivar = cultivars
         .find(item => 
-            normalizeCultivarName(item.name_of_cultivar) === normalizeCultivarName(row[1]['value']) 
-            || normalizeCultivarName(item.breeding_number) === normalizeCultivarName(row[1]['value'])
+            normalizeCultivarName(item.name_of_cultivar) === normalizeCultivarName(cultivarName) 
+            || normalizeCultivarName(item.breeding_number) === normalizeCultivarName(cultivarName)
             || item.mistakes
                     ?.split(';')
                     .map(mistake => normalizeCultivarName(mistake))
-                    .some(mistake => mistake === normalizeCultivarName(row[1]['value'])));
+                    .some(mistake => mistake === normalizeCultivarName(cultivarName)));
     if (currentCultivar) {
         return currentCultivar;
     }
+    const fuzzyCultivar = cultivars
+        .find(item => {
+            // console.log(fuzzy(normalizeCultivarName(item.name_of_cultivar), normalizeCultivarName(cultivarName)))
+            // console.log(fuzzy(normalizeCultivarName(item.breeding_number), normalizeCultivarName(cultivarName)))
+            if (item.name_of_cultivar &&  fuzzy(normalizeCultivarName(item.name_of_cultivar), normalizeCultivarName(cultivarName)) === 1) {
+                return true;
+            }
+            if (item.breeding_number && fuzzy(normalizeCultivarName(item.breeding_number), normalizeCultivarName(cultivarName)) === 1) {
+                return true;
+            }
+            return item.mistakes 
+                ? item.mistakes
+                        .split(';')
+                        .map(mistake => normalizeCultivarName(mistake))
+                        .some(mistake => fuzzy(mistake, normalizeCultivarName(cultivarName)) === 1)
+                : false;
+        });
+
+    return fuzzyCultivar;
 }
 
 const parser = {
